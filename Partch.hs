20c55e7a3d92d032043cdad455ac937412d230e1
Darren M. Struthers <dstruthers@gmail.com>
2012-09-11 13:09:01 -0700

    Cleaned code up a little; revised diamond function

--- Partch.hs
+++ Partch.hs
@@ -32,7 +32,7 @@ octaveUp (Interval r) = Interval $ (2 * numerator r) % denominator r
 octaveDown (Interval r) = Interval $ numerator r % (denominator r * 2)
 
 isSimple :: Interval -> Bool
-isSimple (Interval r) = r >= 1 && r < 2
+isSimple i = i >= 1 && i < 2
 
 simplify :: Interval -> Interval
 simplify i = if isSimple i
@@ -50,10 +50,10 @@ utones n = map utone . filter odd $ [1..n]
 
 scale :: Int -> [Interval]
 scale n = nub $ sort [x * y | x <- otones n, y <- utones n]
-
-diamond :: Int -> Diamond
-diamond n = listArray ((0,0), (n `div` 2, n `div` 2)) 
-                 [otone x * utone y | x <- filter odd [1..n], y <- filter odd [1..n]]
+            
+diamond n = let boundary    = ((0,0), (n `div` 2, n `div` 2))
+                otonalities = sort $ map otone $ filter odd [1..n]
+            in  listArray boundary [x * y | x <- otonalities, y <- map inverse otonalities]
             
 limit :: Diamond -> Int
 limit d = (snd . snd . bounds $ d) * 2 + 1
@@ -64,23 +64,23 @@ cents (Interval r) = let approxRatio = (fromIntegral . numerator $ r) / (fromInt
 
 showDiamond :: Diamond -> String
 showDiamond d = concat . map showRow . showOffsets $ d
-  where showRow r = rowPadding r ++ (concat $ map (showCol . (d!)) r) ++ "\n"
-        maxCols = maximum . map length . showOffsets $ d
+  where showRow r    = rowPadding r ++ (concat $ map (showCol . (d!)) r) ++ "\n"
+        maxCols      = maximum . map length . showOffsets $ d
         rowPadding r = replicate ((maxCols - length r) * maxColSize `div` 2) ' '
-        maxColSize = (maximum $ map (length . show) (elems d)) * 2
-        showCol c = let colSize = length . show $ c
-                        spacesRight = (maxColSize - colSize) `div` 2
-                        spacesLeft = spacesRight + (maxColSize - colSize) `mod` 2
-                    in (replicate spacesLeft ' ') ++ show c ++ (replicate spacesRight ' ')
+        maxColSize   = (maximum $ map (length . show) (elems d)) * 2
+        showCol c    = let colSize = length . show $ c
+                           spacesRight = (maxColSize - colSize) `div` 2
+                           spacesLeft = spacesRight + (maxColSize - colSize) `mod` 2
+                       in (replicate spacesLeft ' ') ++ show c ++ (replicate spacesRight ' ')
                        
 showOffsets :: Diamond -> [[(Int, Int)]]
 showOffsets d = map getRow [1..limit d]
-  where maxCols = limit d `div` 2 + 1
+  where maxCols       = limit d `div` 2 + 1
         distFromCtr n = abs (maxCols - n)
-        numCols n = maxCols - distFromCtr n
-        swap (x,y) = (y,x)
-        getRow n = let colFn = if n > maxCols
-                               then colAt n
-                               else swap . colAt n
-                   in map colFn [0..numCols n - 1]
+        numCols n     = maxCols - distFromCtr n
+        swap (x,y)    = (y,x)
+        getRow n      = let colFn = if n > maxCols
+                                    then colAt n
+                                    else swap . colAt n
+                        in map colFn [0..numCols n - 1]
         colAt row col = (0 + col, distFromCtr row + col)
